88f181bdf3cdbff4dabb86fcdd0e327174c8d298
abhijeet1403 <abhijeet1abhijeet@gmail.com>
2018-03-22 02:13:34 +0530

    cookie issue fixed

--- src/app/_services/cookieUtils/cookie-utils.service.ts
+++ src/app/_services/cookieUtils/cookie-utils.service.ts
@@ -1,47 +1,44 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
-import { CookieService } from 'ngx-cookie';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
-
+import { CookieUtilsBrowserService } from '../cookieUtils-browser/cookie-utils-browser.service';
 import * as moment from 'moment';
+import { Injector } from '@angular/core';
 
 @Injectable()
 export class CookieUtilsService {
 
-  constructor(private _cookieService: CookieService,
-    @Inject(PLATFORM_ID) private platformId: Object
+  private _cookieUtilsBrowserService: CookieUtilsBrowserService;
+  private isBrowser: boolean;
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private injector: Injector
   ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    if (this.isBrowser) {
+      this._cookieUtilsBrowserService = <CookieUtilsBrowserService>this.injector.get(CookieUtilsBrowserService);
+    }
   }
 
   public getValue(key: string) {
-    const cookieValueCrypt = this._cookieService.get(key);
-    if (cookieValueCrypt) {
-      const cookieValue = cookieValueCrypt.split(/[ \:.]+/);
-      console.log('getting cookie value of ' + key + ' as : ' + cookieValue);
-      return cookieValue.length > 1 ? cookieValue[1] : cookieValue[0];
-    } else {
+
+    if (this.isBrowser) {
+      return this._cookieUtilsBrowserService.getValue(key);
+    }
+    if (!this.isBrowser) {
       return '';
     }
   }
 
   public setValue(name: string, value: string) {
-    this._cookieService.remove(name, {
-      domain: 'localhost',
-      path: '/'
-    });
-    this._cookieService.put(name, value, {
-      domain: 'localhost',
-      path: '/',
-      expires: moment().add(2, 'days').toDate()
-    });
-
-    // moment().add(2, 'days').toDate(), '/', 'localhost'
+    if (this.isBrowser) {
+      this._cookieUtilsBrowserService.setValue(name, value);
+    }
   }
 
   public deleteValue(key) {
-    this._cookieService.remove(key, {
-      domain: 'localhost',
-      path: '/'
-    });
+    if (this.isBrowser) {
+      this._cookieUtilsBrowserService.deleteValue(key);
+    }
   }
 
 }
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -4,7 +4,7 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
-import { CookieService, CookieBackendService } from 'ngx-cookie';
+// import { CookieService, CookieBackendService } from 'ngx-cookie';
 
 
 @NgModule({
@@ -14,12 +14,11 @@ import { CookieService, CookieBackendService } from 'ngx-cookie';
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
-    ServerTransferStateModule,
+    ServerTransferStateModule
   ],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
   bootstrap: [AppComponent],
-  providers: [{ provide: CookieService, useClass: CookieBackendService }], // <--- CHANGES * * * * *
 })
 export class AppServerModule {
 
